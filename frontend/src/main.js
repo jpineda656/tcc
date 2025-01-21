d4496a5d5fe63cf91f79b0372bcb77b4b7a64176
Jonathan Pineda <jcthacar@gmail.com>
2025-01-20 21:48:25 -0300

    se crea el frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,19 @@
+// src/main.js
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router";
+import pinia from "@/store";
+import { useAuthStore } from "@/store/auth";
+
+const app = createApp(App);
+
+// Configuración del store y router
+app.use(pinia);
+app.use(router);
+
+// Inicializar la sesión del usuario
+const authStore = useAuthStore();
+authStore.initializeStore();
+
+// Montar la aplicación
+app.mount("#app");
